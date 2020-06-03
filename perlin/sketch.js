d24cf81307bec4f1503389b59670cd2c41d61703
Joseph Davidson <Joseph.Davidson@magairports.com>
2020-06-03 14:40:23 +0100

    Perlin

--- perlin/sketch.js
+++ perlin/sketch.js
@@ -6,18 +6,44 @@ let s2;
 
 function setup() {
   createCanvas(500, 500);
-  s = createSlider(0, 1, 0.1);
-  s2 = createSlider(0, 100, 0.1);
+  s = createSlider(0, 8, 1);
+  s2 = createSlider(0, 1, 0.01);
 }
-function hexToRgb(hex) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-      }
-    : null;
+function HSVtoRGB(h, s, v) {
+  var r, g, b, i, f, p, q, t;
+  if (arguments.length === 1) {
+    (s = h.s), (v = h.v), (h = h.h);
+  }
+  i = Math.floor(h * 6);
+  f = h * 6 - i;
+  p = v * (1 - s);
+  q = v * (1 - f * s);
+  t = v * (1 - (1 - f) * s);
+  switch (i % 6) {
+    case 0:
+      (r = v), (g = t), (b = p);
+      break;
+    case 1:
+      (r = q), (g = v), (b = p);
+      break;
+    case 2:
+      (r = p), (g = v), (b = t);
+      break;
+    case 3:
+      (r = p), (g = q), (b = v);
+      break;
+    case 4:
+      (r = t), (g = p), (b = v);
+      break;
+    case 5:
+      (r = v), (g = p), (b = q);
+      break;
+  }
+  return {
+    r: r * 255,
+    g: g * 255,
+    b: b * 255,
+  };
 }
 
 function draw() {
@@ -27,12 +53,8 @@ function draw() {
   for (let x = 0; x < width; x += 1) {
     let yOff = 0;
     for (let y = 0; y < width; y += 1) {
-      const randomColor =
-        "#" +
-        (0x1000000 + noise(xOff, yOff, angle) * 0xffffff)
-          .toString(16)
-          .substr(1, 6);
-      const rgb = hexToRgb(randomColor);
+      const randomColor = noise(xOff, yOff, angle) * 255;
+      const rgb = HSVtoRGB(randomColor, 255, 255);
       const index = (x + y * width) * 4;
       pixels[index + 0] = rgb.r;
       pixels[index + 1] = rgb.g;
