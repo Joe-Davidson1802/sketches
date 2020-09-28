df0415920e24354d3283e10e8547a8d0f867dbda
Joe Davidson <joe.davidson@infinityworks.com>
2020-09-28 20:16:11 +0100

    Update home page

--- genHomePage.js
+++ genHomePage.js
@@ -5,7 +5,7 @@ let file = "";
 fs.readdir(".", (err, dirs) => {
   for (let d of dirs) {
     if (d !== ".git") {
-      file += `<a href="/sketches/${d}">${d}</a></br>`;
+      file += `<a href="/sketches/${d}/index.html">${d}</a></br>`;
     }
   }
   console.log(file);
